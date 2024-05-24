e1e8b73ff36992b8630c1e98cc14cc83a6f3e9d0
yousefturin <yusefturin@gmail.com>
2024-05-25 02:26:56 +0300

    chore: Add moti, react-native-reanimated, and expo-linear-gradient dependencies

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { Skeleton } from 'moti/skeleton'
 
 export default function App() {
+  const SkeletonCommonProps = {
+    colorMode: 'dark', 
+    backgroundColor: "#333",
+    transition:{
+        opacity: {
+            type: "timing",
+            duration: 1000,
+        }
+    }
+}
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+        <Skeleton
+          show
+          height={35}
+          width={35}
+          radius={'round'}
+          {...SkeletonCommonProps}
+        />
   );
 }
 
